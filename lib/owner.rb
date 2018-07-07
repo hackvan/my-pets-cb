95eaeb12ef5e1841614dd2993304937e5183ac08
Diego Camacho <hackvan@gmail.com>
2018-07-06 20:51:40 -0500

    Initial commit

---
+++ lib/owner.rb
@@ -0,0 +1,63 @@
+class Owner
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def initialize(species)
+    @species = species
+    @pets = { fishes: [], cats: [], dogs: [] }
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each { |dog| dog.mood = "happy" }
+  end
+
+  def play_with_cats
+    @pets[:cats].each { |cat| cat.mood = "happy" }
+  end
+
+  def feed_fish
+    @pets[:fishes].each { |fish| fish.mood = "happy" }
+  end
+
+  def sell_pets
+    @pets.each do |type, pets|
+      pets.map { |pet| pet.mood = "nervous" }
+      pets.clear
+    end
+  end
+
+  def list_pets
+    return "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+end
